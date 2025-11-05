38284a412b247d0a5a86267ebdee70c65b5157cf
Gabriel <geonelson06@gmail.com>
2025-11-05 02:09:59 -0500

    2-1

--- src/debug.js
+++ src/debug.js
@@ -2,21 +2,31 @@
 // Can you make it private using closure?
 
 const createCourse = (topic, instructor) => {
+  // students array is now private to this function scope
+  const students = [];
+
   return {
     topic,
     instructor,
-    students: [],
+    
     addStudent(name) {
-      this.students.push(name);
+      students.push(name);
     },
+
     removeStudent(name) {
-      this.students.splice(this.students.indexOf(name), 1);
+      const index = students.indexOf(name);
+      if (index !== -1) {
+        students.splice(index, 1);
+      }
     },
+
     getStudents() {
-      return this.students;
-    },
+      // return a copy to prevent external mutation
+      return [...students];
+    }
   };
 };
+
 
 module.exports = {
   createCourse,
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,25 +1,79 @@
 const makeIdFunc = () => {
-
+  let id = 0;
+  return () => {
+    id += 1;
+    return id;
+  };
 };
 
 const makePasswordChecker = (correctPassword) => {
+  let attempts = 0;
+  let locked = false;
 
+  return (guess) => {
+    if (locked) return "Account locked";
+
+    if (guess === correctPassword) return true;
+
+    attempts++;
+    if (attempts >= 3) {
+      locked = true;
+      return "Account locked";
+    }
+
+    return false;
+  };
 };
 
 const makeMultiplier = (multiplier) => {
-
+  return (nums) => nums.map(num => num * multiplier);
 };
 
-const makeFilterByLength = (length) => {
-
+const makeFilterByLength = (maxLength) => {
+  return (strings) => strings.filter(str => str.length <= maxLength);
 };
 
 const makeGradeTracker = () => {
+  const grades = [];
 
+  return {
+    addGrade(grade) {
+      if (grade < 0 || grade > 100) return false;
+      grades.push(grade);
+      return true;
+    },
+    getAverage() {
+      if (grades.length === 0) return 0;
+      const total = grades.reduce((sum, g) => sum + g, 0);
+      return total / grades.length;
+    }
+  };
 };
 
 const makeShoppingList = () => {
+  const items = [];
 
+  return {
+    getItems() {
+      return [...items];
+    },
+    addItem(item) {
+      items.push(item);
+      console.log(`${item} successfully added! Now you have ${items.length} item(s).`);
+      return items.length;
+    },
+    removeItem(item) {
+      const index = items.indexOf(item);
+      if (index !== -1) {
+        items.splice(index, 1);
+        console.log(`${item} successfully removed. You now have ${items.length} item(s).`);
+        return true;
+      } else {
+        console.log(`${item} not found.`);
+        return false;
+      }
+    }
+  };
 };
 
 module.exports = {
